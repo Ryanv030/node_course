75eb6d9b7e634ba25d9cee139131bb983fdb3a11
Ryan Palermo <rpalermo22@gmail.com>
2019-08-04 13:36:14 -0700

    updated eslint rules, everything working corectly now.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,19 @@
 module.exports = {
   env: {
     browser: true,
-    es6: true,
+    es6: true
   },
-  extends: [
-    'airbnb-base',
-  ],
+  extends: ['airbnb-base'],
   globals: {
     Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly',
+    SharedArrayBuffer: 'readonly'
   },
   parserOptions: {
     ecmaVersion: 2018,
-    sourceType: 'module',
+    sourceType: 'module'
   },
   rules: {
-  },
+    'arrow-parens': [2, 'as-needed'],
+    'consistent-return': 0
+  }
 };
--- module_1/routes.js
+++ module_1/routes.js
@@ -1,14 +1,13 @@
 const fs = require('fs');
 
-function requestHandler(req, res) {
-  const url = req.url;
-  const method = req.method;
+const requestHandler = (req, res) => {
+  const { method, url } = req;
 
   if (url === '/') {
     res.write('<html>');
     res.write('<head><title>My Second Page</title></head>');
     res.write(
-      '<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Submit</button></form></body>'
+      '<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Submit</button></form></body>',
     );
     res.write('</html>');
     return res.end();
@@ -16,13 +15,12 @@ function requestHandler(req, res) {
   if (url === '/message' && method === 'POST') {
     const body = [];
     req.on('data', chunk => {
-      console.log(chunk);
       body.push(chunk);
     });
     return req.on('end', () => {
       const parsedBody = Buffer.concat(body).toString();
       const message = parsedBody.split('=')[1];
-      fs.writeFile('message.txt', message, err => {
+      fs.writeFile('message.txt', message, () => {
         res.statusCode = 302;
         res.setHeader('Location', '/');
         return res.end();
@@ -33,6 +31,6 @@ function requestHandler(req, res) {
   res.write('<html><head><title>My Second Page</title></head>');
   res.write('<body><h1>Hi from server land</h1></body></html>');
   res.end();
-}
+};
 
 module.exports = requestHandler;
